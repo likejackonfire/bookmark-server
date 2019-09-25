ee49f3c41d6530bcc87b8997e1cf9f2a785d2717
likejackonfire <gravitybringsmedown@gmail.com>
2019-09-24 18:15:24 -0700

    failing tests

---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,34 @@
+function makeBookmarksArray() {
+    return [
+      {
+        id: 1,
+        title: 'Something 1',
+        url: 'https://www.something.org',
+        desc: 'Sample Bookmark description text.',
+        rating: 4
+      },
+      {
+        id: 2,
+        title: 'Something 2',
+        url: 'https://www.something.org',
+        desc: 'Sample Bookmark description text.',
+        rating: 2
+      },
+      {
+        id: 3,
+        title: 'Something 3',
+        url: 'https://www.something.org',
+        desc: 'Sample Bookmark description text.',
+        rating: 3
+      },
+      {
+        id: 4,
+        title: 'Something 4',
+        url: 'https://www.something.org',
+        desc: 'Sample Bookmark description text.',
+        rating: 1
+      }
+    ]
+  }
+
+  module.exports = {makeBookmarksArray}
